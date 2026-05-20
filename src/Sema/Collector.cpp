3e67ab17019ff27e048a535018a64283841af71e
Wizzerinus <quillinde@gmail.com>
2026-05-20 17:59:10 +0300

    fix with collector

--- src/Sema/Collector.cpp
+++ src/Sema/Collector.cpp
@@ -1175,7 +1175,7 @@ void Collector::BuildSymbolTable(ASTContext& ctx, Ptr<Node> node, bool buildTrie
             for (auto& child : iwe->children) {
                 BuildSymbolTable(ctx, child.get(), buildTrie);
             }
-            BuildSymbolTable(ctx, iwe->desugarExpr.get(), buildTrie);
+            BuildSymbolTable(ctx, iwe->body.get(), buildTrie);
             scopeManager.FinalizeScope(ctx);
             break;
         }
